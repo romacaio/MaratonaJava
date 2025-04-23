e68973748d084d598445f088508309e4ea81dc6c
romacaio <romacaio12@gmail.com>
2025-04-23 15:47:25 -0300

    Arquivos Iniciais de Estudo

---
+++ src/academy/devdojo/maratona/java/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratona.java.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args) {
+      // Isso é um comentário de uma linha
+
+        /*
+             Isso é um comentário de múltiplas linhas
+         */
+
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
---
+++ src/academy/devdojo/maratona/java/introducao/Aula02TiposPrimitvos.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratona.java.introducao;
+
+public class Aula02TiposPrimitvos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+       long numeroGrande = 100000;
+        int idade = 118;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500.0f;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        System.out.println("A idade é: " + idade + "anos");
+        System.out.println(verdadeiro);
+        System.out.println("Char: " + caractere );
+
+    }
+}
