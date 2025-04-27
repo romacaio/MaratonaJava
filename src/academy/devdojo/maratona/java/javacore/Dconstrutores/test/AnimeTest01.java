99fc300b81ae2f4dec7765accdd5984b08bba264
romacaio <romacaio12@gmail.com>
2025-04-27 15:04:44 -0300

    feature: Aula59 - Construtores pt02

--- src/academy/devdojo/maratona/java/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratona/java/javacore/Dconstrutores/test/AnimeTest01.java
@@ -1,13 +1,12 @@
 package academy.devdojo.maratona.java.javacore.Dconstrutores.test;
+
 import academy.devdojo.maratona.java.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
-        anime.imprime();
-        anime2.imprime();
 
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação", "ghibli");
+        anime.imprime();
 
     }
 }
