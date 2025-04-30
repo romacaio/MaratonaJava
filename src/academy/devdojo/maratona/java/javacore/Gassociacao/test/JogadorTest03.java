2322204383dd98f9476e991045c595eb2aa55f21
Caio Romão <romacaio12@gmail.com>
2025-04-30 10:17:58 -0300

    feature: Aula 67 - Associação pt04

---
+++ src/academy/devdojo/maratona/java/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratona.java.javacore.Gassociacao.test;
+
+import academy.devdojo.maratona.java.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratona.java.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pelé");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador, jogador2};
+
+        jogador.setTime(time);
+        jogador2.setTime(time);
+        time.setJogadores(jogadores);
+
+        System.out.println("--- Jogador ---");
+        jogador.imprime();
+        System.out.println("--- Time ---");
+        time.imprime();
+    }
+}
