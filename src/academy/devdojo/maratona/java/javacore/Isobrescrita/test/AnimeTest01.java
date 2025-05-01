18d06b9f720b049ebdbd2943553ecf38ff72d375
Caio Romão <romacaio12@gmail.com>
2025-05-01 18:54:11 -0300

    feature: Aula 76 - Sobrescrita do método toStrring

---
+++ src/academy/devdojo/maratona/java/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratona.java.javacore.Isobrescrita.test;
+
+import academy.devdojo.maratona.java.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
