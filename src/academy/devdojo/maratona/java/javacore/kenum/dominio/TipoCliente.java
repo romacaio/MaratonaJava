d9f529c1bdbe1aacf26b9848d41ea231494c8e10
Caio Romão <romacaio12@gmail.com>
2025-05-02 09:49:36 -0300

    feature: Aula 81 - Enumeração pt02

--- src/academy/devdojo/maratona/java/javacore/kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/maratona/java/javacore/kenum/dominio/TipoCliente.java
@@ -1,6 +1,21 @@
 package academy.devdojo.maratona.java.javacore.kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+    public final int VALOR;
+    private String nomeRelatorio;
+
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.VALOR = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public int getVALOR() {
+        return VALOR;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
