0c49ddf7514a195a686815fa20262cf30abddca6
Caio Romão <caiodev14@gmail.com>
2025-05-20 20:39:16 -0300

    feature: Aula 108 - Classes Utilitárias (Strings) pt01

---
+++ src/academy/devdojo/maratona/java/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratona.java.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        String nome = "Caio"; // String constant pool
+        String nome2 = "Caio";
+        nome = nome.concat( " César");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+        String nome3 = new String("Caio"); // dentro do heap
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 == nome3.intern()); // dentro do String pool
+    }
+}
